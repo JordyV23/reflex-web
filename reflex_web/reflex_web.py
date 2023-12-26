7bb0ecb96eb4b32dbbcf6f7a942f0ee2f58b1291
JordyV23 <jordyvargas0123@gmail.com>
2023-12-26 15:16:17 -0600

    Estilizado de algunos componentes

--- reflex_web/reflex_web.py
+++ reflex_web/reflex_web.py
@@ -1,18 +1,28 @@
 import reflex as rx
 from reflex_web.components import navbar,footer
 from reflex_web.views import header, links
+from reflex_web.styles import styles
 
 class State(rx.State):
     pass
 
 def index() -> rx.component :
-    return rx.vstack(
+    return rx.box(
         navbar(),
-        header(),
-        links(),
+        rx.center(
+            rx.vstack(
+                header(),
+                links(),
+                max_width=styles.MAX_WIDTH,
+                width="100%",
+                margin_y=styles.Size.BIG.value,
+            ),
+        ),
         footer(),
     )
     
-app = rx.App()
+app = rx.App(
+    style = styles.BASE_STYLE
+)
 app.add_page(index)
 app.compile()
--- reflex_web/views/links/links.py
+++ reflex_web/views/links/links.py
@@ -5,4 +5,5 @@ def links() -> rx.Component:
     return rx.vstack(
         link_button("GitHub","https://github.com/JordyV23"),
         link_button("LinkedIn","https://www.linkedin.com/in/jordy-jhv/"),
+        width="100%"
     )
